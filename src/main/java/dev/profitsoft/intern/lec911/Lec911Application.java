9ce544d9f809386a7d6cd6f84d4ba3d0e4adbd43
Roman Sierkin <serkin99@ukr.net>
2023-01-04 14:25:18 +0200

    initial commit

---
+++ src/main/java/dev/profitsoft/intern/lec911/Lec911Application.java
@@ -0,0 +1,13 @@
+package dev.profitsoft.intern.lec911;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec911Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lec911Application.class, args);
+    }
+
+}
