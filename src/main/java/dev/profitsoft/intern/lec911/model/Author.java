78c5a106c845429eec523c8fc6275a797004acdc
Roman Sierkin <serkin99@ukr.net>
2023-01-04 17:30:17 +0200

    add Jpa annotation

--- src/main/java/dev/profitsoft/intern/lec911/model/Author.java
+++ src/main/java/dev/profitsoft/intern/lec911/model/Author.java
@@ -2,20 +2,31 @@ package dev.profitsoft.intern.lec911.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "author")
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
 
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
+    @Column(name = "last_name", nullable = false)
     private String lastName;
 
+    @OneToMany(mappedBy = "author")
     private Set<Book> books = new HashSet<>();
 
 }
--- src/main/java/dev/profitsoft/intern/lec911/model/Book.java
+++ src/main/java/dev/profitsoft/intern/lec911/model/Book.java
@@ -2,21 +2,33 @@ package dev.profitsoft.intern.lec911.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "book")
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
 
+    @Column(name = "title", nullable = false)
     private String title;
 
+    @Column(name = "isbn", unique = true, nullable = false)
     private String isbn;
 
+    @Column(name = "published_date")
     private LocalDate publishedDate;
 
+    @ManyToOne
     private Author author;
 
 }
