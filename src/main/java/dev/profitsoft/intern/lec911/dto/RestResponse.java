756ec08a43e7a6993455cf79e938cea7248857eb
Roman Sierkin <serkin99@ukr.net>
2023-01-06 20:57:57 +0200

    add RestResponse dto

---
+++ src/main/java/dev/profitsoft/intern/lec911/dto/RestResponse.java
@@ -0,0 +1,10 @@
+package dev.profitsoft.intern.lec911.dto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class RestResponse {
+
+    private final String result;
+
+}
