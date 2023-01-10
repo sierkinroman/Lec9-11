873ee16b2c04194dcd788c3133a165c798786c38
Roman Sierkin <serkin99@ukr.net>
2023-01-10 17:13:22 +0200

    add method to find book by isbn

--- src/main/java/dev/profitsoft/intern/lec911/repository/BookRepository.java
+++ src/main/java/dev/profitsoft/intern/lec911/repository/BookRepository.java
@@ -4,7 +4,11 @@ import dev.profitsoft.intern.lec911.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findByIsbn(String isbn);
 
 }
