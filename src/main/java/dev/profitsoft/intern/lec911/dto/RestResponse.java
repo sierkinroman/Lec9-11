f0ad2ef65b45ac5cab4302c27721ec8d4a5a619f
Roman Sierkin <serkin99@ukr.net>
2023-01-10 17:16:46 +0200

    add annotations

--- src/main/java/dev/profitsoft/intern/lec911/dto/RestResponse.java
+++ src/main/java/dev/profitsoft/intern/lec911/dto/RestResponse.java
@@ -1,7 +1,13 @@
 package dev.profitsoft.intern.lec911.dto;
 
+import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
 
+@Getter
+@Builder
+@Jacksonized
 @RequiredArgsConstructor
 public class RestResponse {
 
