bc10e8b9a98294f14e753d4e0fc2d421b65e9648
Roman Sierkin <serkin99@ukr.net>
2023-01-12 17:27:19 +0200

    add sql test resources

---
+++ src/test/resources/data-test.sql
@@ -0,0 +1,31 @@
+DELETE FROM book;
+DELETE FROM author;
+ALTER TABLE author AUTO_INCREMENT = 1;
+ALTER TABLE book AUTO_INCREMENT = 1;
+
+INSERT INTO author (first_name, last_name) VALUES
+    ('Agatha', 'Christie'),
+    ('Joan', 'Bauer'),
+    ('Kit', 'Pearson'),
+    ('Lawrence', 'Durrell'),
+    ('Luke', 'Hartwell'),
+    ('Mark', 'Doty'),
+    ('Charles', 'Dickens');
+
+INSERT INTO book (title, isbn, published_date, author_id) VALUES
+    ('Being and Time', '9780007119318', '2007-06-04', 1),
+    ('The Goose Girl', '9780553119220', '2008-10-28', 1),
+    ('The Last Lecture', '9780646418438', '2002-10-21', 1),
+    ('The Glass Menagerie', '9780007151660', '2002-08-24', 1),
+    ('Create Your Own Life', '9780670012824', '2011-02-03', 2),
+    ('A Single Glance', '9780698119178', '2001-06-04', 2),
+    ('The Making Of Ajay: Every Man', '9780140322682', '2003-08-31', 3),
+    ('Wicked Surrender', '9780140381665', '1999-05-25', 3),
+    ('The Princesses of Iowa', '9780140319323', '1987-11-25', 3),
+    ('The Owl Keeper', '9780140153170', '1991-12-01', 4),
+    ('The Gladiator Master', '9781400000289', '2002-02-19', 4),
+    ('Green Mars', '9780615722351', '2012-06-30', 5),
+    ('Glittering Images', '9780807066096', '2002-01-19', 6),
+    ('The Lavender Garden', '9780375761140', '2002-09-10', 7),
+    ('Galileo', '9780140436112', '2000-02-24', 7),
+    ('The Golem', '9781434101853', '2008-07-30', 7);
