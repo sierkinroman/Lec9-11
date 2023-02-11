7437812672d76458785530079fe260bd88dc83e4
Roman Sierkin <serkin99@ukr.net>
2023-02-11 12:26:12 +0200

    add CrossOrigin annotation

--- src/main/java/dev/profitsoft/intern/lec911/controller/BookController.java
+++ src/main/java/dev/profitsoft/intern/lec911/controller/BookController.java
@@ -8,6 +8,7 @@ import dev.profitsoft.intern.lec911.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/books")
 @RequiredArgsConstructor
+@CrossOrigin
 public class BookController {
 
     private final BookService bookService;
